52b200653ebe9cfd440cbb9d2346e7ecafade7d6
Jaton David <david.jaton@he-arc.ch>
2025-01-09 10:57:26 +0100

    TODO 3.1 Done

--- api/caffeinecalculatorapp/serializers.py
+++ api/caffeinecalculatorapp/serializers.py
@@ -23,8 +23,37 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 #
 #     def update(self, instance, validated_data):
 #         ...
+#class CaffeineItemSerializer(serializers.Serializer):
+#   name = serializers.CharField(max_length=100)
+#
+#    def create(self, validated_data):
+#        return CaffeineItem.objects.create(**validated_data)
+#    
+#    def update(self, instance, validated_data):
+#        instance.name = validated_data.get("name", instance.name)
+#        instance.description = validated_data.get("description", instance.description)
+#        instance.serving_size_in_ml = validated_data.get("serving_size_in_ml", instance.serving_size_in_ml)
+#        instance.caffeine_amount_in_mg = validated_data.get("caffeine_amount_in_mg", instance.caffeine_amount_in_mg)
+#        instance.save()
+#         return instance
+
+
 
 # TODO-3-1 Réécrire le serializer en héritant de HyperlinkedModelSerializer
+class CaffeineItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        name = serializers.CharField(max_length=100)
+        model = CaffeineItem
+        fields = [
+            "url",
+            "id",
+            "name",
+            "description",
+            "serving_size_in_ml",
+            "caffeine_amount_in_mg",
+            "created",
+            "updated",
+        ]
 
 # TODO-6-2 Créer un nouveau serializer pour le ConsumedItem
 # TODO-6-9 Créer un nouveau serializer héritant de User qui possède davantage de champs (FK)
